fcbd5efa8007e4d2814a633b08fc4a6a0143ee66
Raul Rodriguez <ralex.rdz@gmail.com>
2018-08-29 14:19:30 -0500

    asd

---
+++ index.js
@@ -0,0 +1,63 @@
+let otro = require('./otro')
+
+var x = 2
+
+var y = 'asd'
+
+function suma (num1, num2) {
+    return num1 + num
+}
+
+let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text2 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text3 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text4 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text5 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text6 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text7 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+let text8 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore exercitationem nemo corrupti repellat quasi nam amet doloremque nihil facere eaque natus deserunt voluptates ipsa, expedita optio architecto eius officia ad eligendi odio velit cupiditate debitis. Deleniti expedita voluptates laudantium sequi deserunt, id fugit totam libero ex voluptate, dignissimos consequuntur molestias.'
+
+
+
+
+
+
+
+suma(x, y)
+
+otro.mult(x, y)
